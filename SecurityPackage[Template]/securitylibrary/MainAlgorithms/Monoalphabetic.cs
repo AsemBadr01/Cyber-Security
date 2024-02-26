7ff9f77bb769be1d84bf953d3f817219bab03df9
sohaila11 <106407384+sohaila11@users.noreply.github.com>
2024-02-27 00:49:24 +0200

    MonoAlphabetic done

--- SecurityPackage[Template]/securitylibrary/MainAlgorithms/Monoalphabetic.cs
+++ SecurityPackage[Template]/securitylibrary/MainAlgorithms/Monoalphabetic.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace SecurityLibrary
 {
@@ -10,19 +8,99 @@ namespace SecurityLibrary
     {
         public string Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
-           
+            Dictionary<char, char> map = new Dictionary<char, char>();
+            Dictionary<char, char> mapped = new Dictionary<char, char>();
+            LinkedList<char> list = new LinkedList<char>(new[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' });
+            StringBuilder x = new StringBuilder();
+            plainText = plainText.ToLower();
+            cipherText = cipherText.ToLower();
+            string y;
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                if (map.ContainsKey(plainText[i]))
+                {
+                    continue;
+                }
+                else
+                {
+                    map.Add(plainText[i], cipherText[i]);
+
+                }
+            }
+            for (char i = 'a'; i <= 'z'; i++)
+            {
+                if (!map.ContainsKey(i))
+                {
+                    continue;
+                }
+                else
+                {
+                    mapped.Add(i, map[i]);
+                    if (list.Find(map[i]) != null)
+                    {
+                        LinkedListNode<char> remove = list.Find(map[i]);
+                        list.Remove(remove);
+                    }
+                }
+            }
+
+            for (char i = 'a'; i <= 'z'; i++)
+            {
+                if (!mapped.ContainsKey(i))
+                {
+                    x.Append(list.First.Value);
+                    list.RemoveFirst();
+                }
+                else
+                {
+                    x.Append(mapped[i]);
+
+                }
+            }
+            y = x.ToString();
+            return y;
         }
 
         public string Decrypt(string cipherText, string key)
         {
-            throw new NotImplementedException();
+            Dictionary<char, char> unmap = new Dictionary<char, char>();
+            StringBuilder x = new StringBuilder();
+            string y;
+            int c = 0;
+            cipherText = cipherText.ToLower();
+            key = key.ToLower();
+            for (char i = 'a'; i <= 'z'; i++)
+            {
+                unmap.Add(key[c], i);
+                c++;
+            }
 
+            for (int i = 0; i < cipherText.Length; i++)
+            {
+                x.Append(unmap[cipherText[i]]);
+            }
+            y = x.ToString();
+            return y;
         }
 
         public string Encrypt(string plainText, string key)
         {
-            throw new NotImplementedException();
+            Dictionary<char, char> map = new Dictionary<char, char>();
+            StringBuilder x = new StringBuilder();
+            string y;
+            int c = 0;
+            for (char i = 'a'; i <= 'z'; i++)
+            {
+                map.Add(i, key[c]);
+                c++;
+            }
+
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                x.Append(map[plainText[i]]);
+            }
+            y = x.ToString();
+            return y;
         }
 
 
@@ -66,10 +144,34 @@ namespace SecurityLibrary
 
         public string AnalyseUsingCharFrequency(string cipher)
         {
+            Queue<char> list = new Queue<char>(new[] { 'e', 't', 'a', 'o', 'i', 'n', 's', 'r', 'h', 'l', 'd', 'c', 'u', 'm', 'f', 'p', 'g', 'w', 'y', 'b', 'v', 'k', 'x', 'j', 'q', 'z' });
+            Dictionary<char, int> map = new Dictionary<char, int>();
+            Dictionary<char, char> mapped = new Dictionary<char, char>();
+            StringBuilder z = new StringBuilder();
+            string y;
+            for (int i = 0; i < cipher.Length; i++)
+            {
+                if (map.ContainsKey(cipher[i]))
+                {
+                    map[cipher[i]]++;
+                }
+                else
+                {
+                    map.Add(cipher[i], 1);
+                }
+            }
+            var sortmap = map.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+            foreach (var pair in sortmap)
+            {
+                mapped.Add(pair.Key, list.Dequeue());
+            }
 
-            throw new NotImplementedException();
-
-
+            for (int i = 0; i < cipher.Length; i++)
+            {
+                z.Append(mapped[cipher[i]]);
+            }
+            y = z.ToString();
+            return y;
         }
     }
 }
