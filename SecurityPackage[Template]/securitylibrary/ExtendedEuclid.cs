5a15fddafb62675343606ab7d0ef9cc4392f34a5
sohaila11 <106407384+sohaila11@users.noreply.github.com>
2024-04-02 02:32:45 +0200

    extended euclid done

--- SecurityPackage[Template]/securitylibrary/ExtendedEuclid.cs
+++ SecurityPackage[Template]/securitylibrary/ExtendedEuclid.cs
@@ -1,22 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SecurityLibrary.AES
 {
-    public class ExtendedEuclid 
+    public class ExtendedEuclid
     {
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="number"></param>
-        /// <param name="baseN"></param>
-        /// <returns>Mul inverse, -1 if no inv</returns>
+
         public int GetMultiplicativeInverse(int number, int baseN)
         {
-            throw new NotImplementedException();
+            List<int> Q = new List<int>();
+            List<int> A1 = new List<int>();
+            List<int> A2 = new List<int>();
+            List<int> A3 = new List<int>();
+            List<int> B1 = new List<int>();
+            List<int> B2 = new List<int>();
+            List<int> B3 = new List<int>();
+            A1.Add(1);
+            A2.Add(0);
+            A3.Add(baseN);
+            B1.Add(0);
+            B2.Add(1);
+            B3.Add(number);
+            int i = 0;
+            while (B3.Last() != 1 && B3.Last() != 0)
+            {
+                Q.Add(((A3[i]) / (B3[i])));
+                A1.Add(B1[i]);
+                A2.Add(B2[i]);
+                A3.Add(B3[i]);
+                B1.Add(A1[i] - ((Q[i]) * B1[i]));
+                B2.Add(A2[i] - ((Q[i]) * B2[i]));
+                B3.Add(A3[i] - ((Q[i]) * B3[i]));
+                i++;
+            }
+            if (B3.Last() == 0)
+            {
+                return -1;
+            }
+            else if (B3.Last() == 1)
+            {
+                if (B2.Last() >= 0)
+                {
+                    return B2.Last();
+                }
+                else
+                {
+                    int last = B2.Last();
+                    while (last < 0)
+                    {
+                        last += baseN;
+                    }
+                    return last;
+                }
+            }
+            return 0;
         }
     }
 }
